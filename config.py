0fa98dfc425a093c1e5251ca39d3d1992a497f24
yadneshg <58324817+yadneshg@users.noreply.github.com>
2020-07-20 11:00:45 +0530

    initial commit

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Cyber123@localhost:5432/db01"
-    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Cyber123@localhost:5432/db01"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL1']
 
 
 class ProductionConfig(Config):
